779995adc5aaf048a348e03844caa9cb55154b0c
申舒畅 <china15537079972@163.com>
2022-05-31 19:58:18 +0800

    'dev1.1'

---
+++ js/PFR.js
@@ -0,0 +1,29 @@
+
+function setRem() {
+    var ui_w = 375;
+    // 获取屏幕的宽度
+    var clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    console.log(ui_w, clientWidth);
+
+    // 通过js动态改变html根节点字体大小
+    var html_ = document.getElementsByTagName('html')[0];
+    html_.style.fontSize = (clientWidth / ui_w) * 10 + 'px';
+}
+// window.onresize 浏览器被重置大小执行事件
+window.onresize = setRem;
+
+
+// 滚动替换
+// function sold() {
+//     var scrooltop = document.documentElement.scrollTop || document.body.scrollTop;
+//     var clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+//     var images = document.getElementsByClassName('images');
+//     for (const img of images) {
+//         if ((img.offsetTop + 200) <= (scrooltop + clientHeight)) {
+//             var realSrc = img.getAttribute("data-realSrc");
+//             img.src = realSrc;
+//         }
+//     }
+// }
+// window.onload = sold;
+// window.onscroll = sold;
